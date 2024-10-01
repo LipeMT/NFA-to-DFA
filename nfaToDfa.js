6089306043a77f69cf21a662d1ea9a85dc3d04d8
Luis Felipe Tomé <lipetome010@gmail.com>
2024-09-30 22:43:08 -0300

    Projeto Final

---
+++ nfaToDfa.js
@@ -0,0 +1,84 @@
+import { conjuntoDasPartes, uniaoConjuntos, intersecaoConjuntos } from './util.js';
+import input1 from './input1.json'assert { type: 'json' };
+import input2 from './input2.json' assert { type: 'json' };
+
+let nfa1 = input1;
+let nfa2 = input2;
+
+function criarCombinacoes(dfa) {
+    dfa.state = conjuntoDasPartes(dfa.state);
+}
+
+function definirTransicoes(dfa) {
+    let { transition } = dfa;
+    dfa.transition = {}
+
+    let conjuntoEntradas0 = []
+    let conjuntoEntradas1 = []
+
+    for (let subconjunto of dfa.state) {
+        for (let i = 0; i < subconjunto.length; i++) {
+            conjuntoEntradas0 = uniaoConjuntos(conjuntoEntradas0, transition[subconjunto[i]]['0'])
+            conjuntoEntradas1 = uniaoConjuntos(conjuntoEntradas1, transition[subconjunto[i]]['1'])
+        }
+        dfa.transition[subconjunto] = { '0': conjuntoEntradas0, '1': conjuntoEntradas1 }
+        conjuntoEntradas0 = []
+        conjuntoEntradas1 = []
+    }
+    console.table(dfa.transition)
+}
+
+function definirEstadosFinais(dfa) {
+    let { end_state } = dfa
+    end_state = end_state.toString()
+    dfa.end_state = []
+
+    for (let element in dfa.transition) {
+        if (element.includes(end_state)) {
+            dfa.end_state.push(element)
+        }
+    }
+}
+
+function encontrarEstadosAcessiveis(dfa) {
+    let { state, initial_state, transition } = dfa
+    let acessiveis = [initial_state]
+    
+    for (let i = 0; i < state.length; i++) {
+        acessiveis.forEach(element => {
+            console.log(transition[element[0]])
+            let estado0 = transition[element][0].join(',');
+            if (!acessiveis.includes(estado0)) {
+                acessiveis.push(estado0)
+            }
+
+            let estado1 = transition[element][1].join(',');
+            if (!acessiveis.includes(estado1)) {
+                acessiveis.push(estado1)
+            }
+        })
+    } 
+    dfa.transition = {}
+    for (let estado in transition){
+        for (let estadoAcessivel of acessiveis){
+            if(estado == estadoAcessivel){
+                dfa.transition[estado] = transition[estado]
+            }
+        }
+    }
+    dfa.state = acessiveis;
+    dfa.end_state = intersecaoConjuntos(dfa.state, dfa.end_state)
+    // console.log(dfa.transition)
+}
+
+function converterNfaParaDfa(nfa){
+    criarCombinacoes(nfa)
+    definirTransicoes(nfa)
+    definirEstadosFinais(nfa)
+    encontrarEstadosAcessiveis(nfa)
+    console.log(nfa)
+    console.table(nfa.transition)
+}
+
+converterNfaParaDfa(nfa1)
+converterNfaParaDfa(nfa2)
---
+++ util.js
@@ -0,0 +1,35 @@
+export function conjuntoDasPartes(arr) {
+    let resultado = [[]];
+    for (let elemento of arr) {
+        let novosSubconjuntos = resultado.map(subconjunto => [...subconjunto, elemento]);
+        resultado = [...resultado, ...novosSubconjuntos];
+    }
+    resultado.sort((a, b) => a.length - b.length);
+    return resultado;
+}
+
+export function uniaoConjuntos(conjunto1, conjunto2) {
+    const uniao = new Set([...conjunto1, ...conjunto2]);
+    return [...uniao].sort((a, b) => {
+        const numA = parseInt(a.replace('q', ''), 10);
+        const numB = parseInt(b.replace('q', ''), 10);
+        return numA - numB;
+    });
+}
+
+
+export function intersecaoConjuntos(conjuntoA, conjuntoB) {
+    const intersecao = [];
+
+    for (let elemento of conjuntoA) {
+        if (conjuntoB.includes(elemento)) {
+            intersecao.push(elemento);
+        }
+    }
+
+    return intersecao;
+}
+
+
+
+
